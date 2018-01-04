80c0d2b8c661b9ccb937a2c215b1b2d9385ddd06
Patreev Igor <Patreewigor@yandex.ru>
2018-01-04 23:10:59 +0300

    Delete limit 16 from migration

--- db/migrate/20170826184619_create_items.rb
+++ db/migrate/20170826184619_create_items.rb
@@ -1,11 +1,11 @@
 class CreateItems < ActiveRecord::Migration[5.1]
   def change
     create_table :items do |t|
-      t.integer :class_id, :limit => 16
-      t.integer :instance_id, :limit => 16
+      t.integer :class_id
+      t.integer :instance_id
       t.string  :hash_name
-      t.integer :price, :limit => 16
-      t.float   :coefficient_profit, :limit => 16
+      t.integer :price
+      t.float   :coefficient_profit
       
       t.timestamps
     end
